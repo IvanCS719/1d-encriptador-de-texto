c09d2a6b69bf2ac007df7fa32db8252ef6e2dd88
IvanCS719 <ivancupil17@gmail.com>
2024-02-27 11:40:18 -0600

    Se creó la función que encripta el texto, además se modifico el nombre del objeto pasando de llaves - llavesVocales

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 //Creando un objeto con las vocales y su incritación
-const llaves = {
+const llavesVocales = {
     'e' : "enter",
     'i' : "imes",
     'a' : "ai",
     'o' : "ober",
     'u' : "ufat"
+}
+
+//Función para encriptar el texto del usuario
+//Se recibe el texto como párametro
+function encriptacion(texto) {
+    //Variable para concatenar el texto resultante
+    let resulText = '';
+    
+    //Se recorre el texto como un arreglo
+    for (let i = 0; i < texto.length; i++) {
+        //Se extrae cada caracter del texto
+        const letra = texto[i];
+        
+        /**Si el caracter es alguna clave del objeto llavesVocales
+         * se obtine su valor y se concatena al resulText, en caso contrario
+         * se contena el mismo caracter del texto recorrido.
+        */
+        resulText += llavesVocales[letra] || letra;
+        
+    }
+
+    //Se retorna el texto encriptado
+    return resulText;
 }
